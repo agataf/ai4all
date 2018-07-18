5f7102c62679e208ca210c43ef8b159920509cc4
agataf <agata@foryciarz.com>
2018-07-18 15:47:50 +0200

    edited regression

---
+++ Lectures/ff_functions.py
@@ -0,0 +1,139 @@
+import matplotlib.pyplot as plt
+import numpy as np
+import scipy.stats as stats
+import pandas as pd
+
+'''
+This function removes rows containing NaNs from a 
+DataFrame. Works for both 1D and 2D DataFrames.
+'''
+def remove_nan(data):
+    if len(data.shape)>1:
+        return data[(~np.isnan(data)).all(1)]
+    else:
+        return data[~np.isnan(data)]
+
+'''
+This function removes rows with values below or equal to 0 from a 
+DataFrame. Works for both 1D and 2D DataFrames.
+'''
+def select_above_zero(data):
+    if len(data.shape)>1:
+        return data[(data>0).all(1)]
+    else:
+        return data[data>0]
+
+'''
+This function removes rows with values below 0 from a 
+DataFrame. Works for both 1D and 2D DataFrames.
+'''
+def select_nonnegative(data):
+    if len(data.shape)>1:
+        return data[(data>=0).all(1)]
+    else:
+        return data[data>=0]
+
+'''
+Plot a histogram based on a 1D DataFrame
+'''
+def plot_one_histogram(data, label="", xlabel="", ylabel="", title=""):
+    max_val = int(max(data))
+    min_val = int(min(data))
+    bins = np.linspace(min_val, max_val, min(max_val-min_val,100))
+    fig = plt.figure(figsize=(7,7))
+    plt.hist(data, bins, alpha=0.5, label=label)
+    plt.xlabel(xlabel)
+    plt.ylabel(ylabel)
+    plt.title(title)
+    plt.legend(loc="upper right")
+    plt.show()
+
+'''
+Plot two overlapping histograms based on two 1D DataFrames
+'''
+def plot_two_histograms(data1, data2, label1="", label2="", xlabel="", ylabel="", title=""):
+    max_val = int(max(max(data1),max(data2)))
+    min_val = int(min(min(data1),min(data2)))
+    # the numpy linspace function to create an array of evenly spaced numbers
+    bins = np.linspace(min_val, max_val, min(max_val-min_val,100))
+    # Let's create the matplotlib figure where we will plots the histograms.
+    fig = plt.figure(figsize=(7,7))
+    # Plot the histograms. We use pyplot's hist function and 
+    plt.hist(data1, bins, alpha=0.5, label=label1)
+    plt.hist(data2, bins, alpha=0.5, label=label2)
+    # Label the plot.
+    plt.xlabel(xlabel)
+    plt.ylabel(ylabel)
+    plt.title(title)
+    # Plot a legend so that we can match the color of the histogram to the data.
+    plt.legend(loc="upper right")
+    # Show the plot
+    plt.show()
+
+'''
+This function takes in two arrays of equal length (xdata, ydata)
+and plots them against each other in a scatterplot
+'''
+def scatterplot(xdata, ydata, xlabel="", ylabel="", title=""):
+    fig = plt.figure(figsize=(7,7))
+    data_max = max(max(xdata), max(ydata))
+    plt.scatter(xdata, ydata)
+    #plt.plot([0,data_max],[0,data_max])
+    plt.xlim(0,max(xdata))
+    plt.ylim(0,max(ydata))
+    plt.xlabel(xlabel)
+    plt.ylabel(ylabel)
+    plt.title(title)
+    fig.show()
+
+def two_bar_charts(data1, data2, label1="", label2="", bar_labels=[]):
+    labels = np.arange(4)
+    fig = plt.figure(figsize=(10,5))
+    ax1 = plt.subplot(121) # 121 means we want 1 row, 2 columns, and that we want to plot on the first of these 2 subplots
+    plt.title(label1)
+    ax1.bar(labels, data1)
+    ax2 = plt.subplot(122) # 122 means we want the 2nd plot of the 1 row, 2 columns figure.
+    plt.title(label2)
+    ax2.bar(labels, data2)
+    ax1.set_xticks(labels+0.4)
+    ax1.set_xticklabels(bar_labels)
+    ax2.set_xticks(labels+0.4)
+    ax2.set_xticklabels(bar_labels)
+    plt.show()
+
+def one_bar_chart(data, label="", bar_labels=[]):
+    labels = np.arange(4)
+    fig = plt.figure(figsize=(10,5))
+    ax = plt.subplot(121) # 121 means we want 1 row, 2 columns, and that we want to plot on the first of these 2 subplots
+    plt.title(label)
+    ax.bar(labels, data)
+    ax.set_xticks(labels+0.4)
+    ax.set_xticklabels(bar_labels)
+    plt.show()
+
+def one_pie_chart(data, label, pie_labels=[]):
+    explode = (0, 0, 0, 0.1)
+    plt.rcParams.update({'font.size': 12})
+    fig = plt.figure(figsize=(10,6))
+    ax1 = plt.subplot(121)
+    plt.title(label)
+    ax1.pie(data, explode=explode, labels=pie_labels, autopct='%1.1f%%',
+            shadow=True, startangle=90)
+    ax1.axis('equal')
+    plt.show()
+
+def two_pie_charts(data1, data2, label1, label2, pie_labels=[]):
+    explode = (0, 0, 0, 0.1)
+    plt.rcParams.update({'font.size': 22})
+    fig = plt.figure(figsize=(30,10))
+    ax1 = plt.subplot(121)
+    plt.title("Mother's Education")
+    ax1.pie(data1, explode=explode, labels=pie_labels, autopct='%1.1f%%',
+            shadow=True, startangle=90)
+    ax1.axis('equal')
+    ax2 = plt.subplot(122)
+    plt.title("Father's Education")
+    ax2.pie(data2, explode=explode, labels=pie_labels, autopct='%1.1f%%',
+            shadow=True, startangle=90)
+    ax2.axis('equal')
+    plt.show()
